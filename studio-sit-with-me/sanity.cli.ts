92b959aeb913112ec03591ab738734dc9d8e6d92
SamuelOkewu <okewusamuelowoicho@gmail.com>
2026-03-01 00:29:52 +0100

    Update sanity.cli.ts

--- studio-sit-with-me/sanity.cli.ts
+++ studio-sit-with-me/sanity.cli.ts
@@ -2,10 +2,11 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '9cjzddfw',
+    projectId: 'gy19d8am',
     dataset: 'production'
   },
   deployment: {
+    appId: 'dko97dii59u7wubeprnkrmnn',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
