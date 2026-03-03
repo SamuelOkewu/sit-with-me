0b759ad5fa0dc464592b48db99515bcc0d19bf8e
SamuelOkewu <okewusamuelowoicho@gmail.com>
2026-03-03 02:20:20 +0100

    Configuring to Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.VITE_SANITY_PROJECT_ID': JSON.stringify(env.VITE_SANITY_PROJECT_ID),
+      'process.env.VITE_SANITY_DATASET': JSON.stringify(env.VITE_SANITY_DATASET),
+      'process.env.VITE_SANITY_TOKEN': JSON.stringify(env.VITE_SANITY_TOKEN),
     },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
-    server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
+
   };
 });
